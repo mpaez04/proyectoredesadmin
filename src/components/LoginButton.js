8dc88282535254e879b51265b1a01fabfbc803c7
Miguel Paez <miguel.paez18@tectijuana.edu.mx>
2022-05-25 10:57:47 -0700

    se modifico la autentificacion de administrador

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import "./LoginButton.css";
+import General from "./General";
+
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
+  const {general} = General();
   return (
     <div className="LoginButton">
-      <button onClick={() => loginWithRedirect()}>login</button>
+      <button onClick={() => loginWithRedirect()}>login Administrador</button>
     </div>
   );
 };
